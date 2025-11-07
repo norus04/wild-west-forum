61b9ea68ce10117b630d1546691881c9c3bca758
norus04 <noah.rushing@maine.edu>
2025-11-07 18:38:27 -0500

    Basic set up of handlebars, body parsing, & cookies

--- app/server.js
+++ app/server.js
@@ -1,7 +1,31 @@
 const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// In-memory
+const users = [];
+const comments = [];
+const sessions = {};
+
+// View engine
+app.engine('hbs', exphbs.engine({
+  extname: 'hbs',
+  defaultLayout: 'main',
+  layoutsDir: path.join(__dirname, 'views', 'layouts'),
+  partialsDir: path.join(__dirname, 'views', 'partials')
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Basic middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.send('Wild West Forum is running.');
