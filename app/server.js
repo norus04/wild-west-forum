621a09a7d64209ad2a038ba69ff968eb1b6b91ee
norus04 <noah.rushing@maine.edu>
2025-11-07 23:47:02 -0500

    Port change

--- app/server.js
+++ app/server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 // In-memory
 const users = [];
